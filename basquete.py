f131454eb0e7f1aeff562a082dab043a2db0fe83
miachafer <miachafer@gmail.com>
2021-11-10 17:47:11 -0300

    inserindo função que cria um dicionario referente ao novo jogo inserido

--- basquete.py
+++ basquete.py
@@ -5,3 +5,39 @@ tabela = [
     {'Jogo': 4, 'Placar': 24, 'Min. temporada': 10, 'Max temporada': 24, 'Quebra recorde min.': 1, 'Quebra recorde máx.': 1}
 ]
 
+def basquete():
+    for chave, valor in tabela[-1].items():
+        if chave == 'Jogo':
+            jogo = valor + 1
+            placar = int(input(f'Insira o placar do {jogo}º jogo '))
+
+        if chave == 'Min. temporada':
+            if placar >= valor:
+                min_temporada = valor
+            else:
+                min_temporada = placar
+
+        if chave == 'Max temporada':
+            if placar > valor:
+                max_temporada = placar
+            else:
+                max_temporada = valor
+
+        if chave == 'Quebra recorde min.':
+            if min_temporada == placar:
+                quebra_min = valor + 1
+            else:
+                quebra_min = valor
+
+        if chave == 'Quebra recorde máx.':
+            if max_temporada == placar:
+                quebra_max = valor + 1
+            else:
+                quebra_max = valor
+
+    novo_jogo = {'Jogo': jogo, 'Placar': placar, 'Min. temporada': min_temporada, 'Max temporada': max_temporada, \
+                 'Quebra recorde min.': quebra_min, 'Quebra recorde máx.': quebra_max}
+    tabela.append(novo_jogo)
+    print(tabela)
+
+basquete()
